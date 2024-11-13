386cb435f361ebd82753e5d0c0f6f84e6e93b579
Nalini Chandhi <nalini.chandhi@microsoft.com>
2024-11-12 20:33:25 -0800

    file path change2

--- Deployment/scripts/copy_kb_files.sh
+++ Deployment/scripts/copy_kb_files.sh
@@ -28,13 +28,12 @@ unzip /mnt/azscripts/azscriptinput/"$zipFileName1" -d /mnt/azscripts/azscriptinp
 unzip /mnt/azscripts/azscriptinput/"$zipFileName2" -d /mnt/azscripts/azscriptinput/"$extractedFolder2"
 unzip /mnt/azscripts/azscriptinput/"$zipFileName3" -d /mnt/azscripts/azscriptinput/"$extractedFolder3"
 
+echo "Script Started"
+sed -i "s/<STORAGE_ACCOUNT_URL_TO_BE_REPLACED>/${storageAccount}/g" "/mnt/azscripts/azscriptinput/ragtest/settings.yaml"
+
 az storage fs directory upload -f "$fileSystem" --account-name "$storageAccount" -s "$extractedFolder1" --account-key "$accountKey" --recursive
 az storage fs directory upload -f "$fileSystem" --account-name "$storageAccount" -s "$extractedFolder2" --account-key "$accountKey" --recursive
 az storage fs directory upload -f "$fileSystem" --account-name "$storageAccount" -s "$extractedFolder3" --account-key "$accountKey" --recursive
-
-echo "Script Started"
-
-sed -i "s/<STORAGE_ACCOUNT_URL_TO_BE_REPLACED>/${storageAccount}/g" "ragtest/settings.yaml"
 
 # pip install graphrag==0.3.6
 
